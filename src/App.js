57ab248c3ebffa1e87c32ccd3be6f5bdd3ba85a7
matthunterking <matthew85king@gmail.com>
2021-11-17 14:57:45 +0000

    Added basic error handling to api request

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { baseApiURL } from './environment';
 const App = () => {
 
   const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
   const [showData, setShowData] = useState(null);
 
   useEffect(() => {
@@ -13,10 +14,16 @@ const App = () => {
       .then(({ data }) => {
         setShowData(data);
         setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
+        setHasError(true);
       });
   }, []);
 
   if (isLoading) return <p>Loading...</p>
+
+  if (hasError) return <p>Oops something has gone wrong. Please try again later</p>
 
   return (
     <div>
